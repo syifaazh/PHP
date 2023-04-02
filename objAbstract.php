8af031a4226ad387f85dbcd65942478fb759e825
Syifa 'Azhima <meutuahsyifa409@gmail.com>
2023-04-02 12:57:45 +0700

    up tugas php 6

--- objAbstract.php
+++ objAbstract.php
@@ -1,7 +1,10 @@
-<br><br>
+<br>
 <center>
-<table border=1>
-    <thead></thead>
+<table border=1 cellpadding=10>
+    <thead>
+        <h2><i>~~~Menghitung Bangun Datar~~~</i></h2>
+        <hr>
+    </thead><br>
     <tbody>
     <?php
         require_once 'Lingkaran.php';
@@ -16,8 +19,8 @@
 
         foreach ($ar_data as $data){
            ?> <tr>
-                <td colspan=2 width=25% align='center' bgcolor=159895> 
-                    <?= $data->namaBidang(). '<br>'?>
+                <td colspan=2 width=25% align='center' bgcolor=159895 > 
+                    <b><i><?= $data->namaBidang(). '<br>'?></i></b>
                 </td>
               </tr>
               <tr align='center' bgcolor=FEFAE0>
