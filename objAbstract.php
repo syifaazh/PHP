01d8ae664d7e08685ec5714cf66ee8815468938d
Syifa 'Azhima <meutuahsyifa409@gmail.com>
2023-04-02 12:49:47 +0700

    upload tugas php 6

--- objAbstract.php
+++ objAbstract.php
@@ -1,3 +1,4 @@
+<br><br>
 <center>
 <table border=1>
     <thead></thead>
@@ -7,21 +8,23 @@
         require_once 'persegiPanjang.php';
         require_once 'segitiga.php';
 
-        $l1 = new Lingkaran(78);
-        $p1 = new persegiPanjang (14,34);
-        $s1 = new Segitiga(3,4,5);
+        $l1 = new Lingkaran(16);
+        $p1 = new persegiPanjang (50,24);
+        $s1 = new Segitiga(8,4,4);
 
         $ar_data = [$l1, $p1, $s1];
 
         foreach ($ar_data as $data){
            ?> <tr>
-                <td colspan=2 width=30% align='center'> <?= $data->namaBidang(). '<br>'?></td>
+                <td colspan=2 width=25% align='center' bgcolor=159895> 
+                    <?= $data->namaBidang(). '<br>'?>
+                </td>
               </tr>
-              <tr align='center'>
+              <tr align='center' bgcolor=FEFAE0>
                 <td>Luas</td>
                 <td><?= $data->LuasBidang() ?> </td>
               </tr>
-              <tr align='center'>
+              <tr align='center' bgcolor=FEFAE0>
                 <td>Keliling</td>
                 <td><?= $data->kelilingBidang() ?> </td>
               </tr>
