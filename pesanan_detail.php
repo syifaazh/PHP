16e0144617ab1ca87e5cae570acafa798818f13a
Syifa 'Azhima <meutuahsyifa409@gmail.com>
2023-05-11 02:42:03 +0700

    TUGAS 12

--- pesanan_detail.php
+++ pesanan_detail.php
@@ -4,24 +4,24 @@ $model = new Pesanan();
 $pesanan = $model->getPesanan($id);
 
 ?>
-    <h1 class="mt-4">Tabel Pesanan</h1>
-    <div class="card-body">
-        <div class="card mb-4">
-            <table id="datatablesSimple">
-                <thead>
-                    <tr>
-                        <th>Tanggal</th>
-                        <th>Total</th>
-                        <th>Id pelanggan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><?= $pesanan['tanggal'] ?></td>
-                        <td><?= $pesanan['total'] ?></td>
-                        <td><?= $pesanan['pelanggan_id'] ?></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+<h1 class="mt-4">Tabel Pesanan</h1>
+<div class="card-body">
+    <div class="card mb-4">
+        <table id="datatablesSimple">
+            <thead>
+                <tr>
+                    <th>Tanggal</th>
+                    <th>Total</th>
+                    <th>Id pelanggan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?= $pesanan['tanggal'] ?></td>
+                    <td><?= $pesanan['total'] ?></td>
+                    <td><?= $pesanan['pelanggan_id'] ?></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+</div>
